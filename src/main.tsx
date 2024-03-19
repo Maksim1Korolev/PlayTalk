0cab790d83c57c916ad2e0fa2fa182bbf1c814b8
Maksim Korolev <kimikirimusu@gmail.com>
2024-03-19 16:51:00 +0200

    StoreProvider added

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { StoreProvider } from "./app/providers/store/StoreProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <StoreProvider>
+      <App />
+    </StoreProvider>
   </BrowserRouter>
 );
