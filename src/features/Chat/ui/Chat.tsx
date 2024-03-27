2761c6cd36f5e971450ec5ed4f6eefbd95a1db28
Maksim Korolev <kimikirimusu@gmail.com>
2024-03-27 18:05:01 +0200

    small fix

--- src/features/Chat/ui/Chat.tsx
+++ src/features/Chat/ui/Chat.tsx
@@ -1,63 +1,70 @@
-import { useState } from 'react'
-import { cx } from '../../../shared/lib/cx'
-import { Card, HStack, UiButton, UiInput, UiText, VStack } from '../../../shared/ui'
-import cls from './Chat.module.scss'
+import { useState } from "react";
+import { cx } from "../../../shared/lib/cx";
+import {
+  Card,
+  HStack,
+  UiButton,
+  UiInput,
+  UiText,
+  VStack,
+} from "../../../shared/ui";
+import cls from "./Chat.module.scss";
 interface MessageHistory {
-	message: string
-	date: Date
-	username: string
+  message: string;
+  date: Date;
+  username: string;
 }
 
 export const Chat = ({
-	className,
-	currentUsername,
-	receiverUsername,
-	handleSendMessage,
-	onMessageReceived,
+  className,
+  currentUsername,
+  receiverUsername,
+  handleSendMessage,
+  onMessageReceived,
 }: {
-	className?: string
-	currentUsername: string
-	receiverUsername: string
-	onMessageReceived: (message: string) => void
-	handleSendMessage: (message: string) => void
+  className?: string;
+  currentUsername: string;
+  receiverUsername: string;
+  onMessageReceived: (message: string) => void;
+  handleSendMessage: (message: string) => void;
 }) => {
-	const [messageHistory, setMessageHistory] = useState<MessageHistory[]>([])
-	const [inputMessage, setInputMessage] = useState<string>('')
+  const [messageHistory, setMessageHistory] = useState<MessageHistory[]>([]);
+  const [inputMessage, setInputMessage] = useState<string>("");
 
-	const AddMessageToHistory = (username: string, message: string) => {
-		const newMessage: MessageHistory = {
-			message: message,
-			date: new Date(),
-			username: username,
-		}
-		setMessageHistory(prev => ({
-			...prev,
-			newMessage,
-		}))
-	}
+  const AddMessageToHistory = (username: string, message: string) => {
+    const newMessage: MessageHistory = {
+      message: message,
+      date: new Date(),
+      username: username,
+    };
+    setMessageHistory((prev) => ({
+      ...prev,
+      newMessage,
+    }));
+  };
 
-	const onMessageReceived = (message: string) => {
-		AddMessageToHistory(receiverUsername, message)
-	}
+  onMessageReceived = (message: string) => {
+    AddMessageToHistory(receiverUsername, message);
+  };
 
-	const handleClickButton = () => {
-		handleSendMessage(inputMessage)
+  const handleClickButton = () => {
+    handleSendMessage(inputMessage);
 
-		AddMessageToHistory(currentUsername, inputMessage)
-		setInputMessage('')
-	}
+    AddMessageToHistory(currentUsername, inputMessage);
+    setInputMessage("");
+  };
 
-	return (
-		<Card className={cx(cls.Chat, {}, [className])}>
-			<VStack className={cls.messageLog}>
-				{messageHistory.map(message => (
-					<UiText>{message.message}</UiText>
-				))}
-			</VStack>
-			<HStack>
-				<UiInput value={inputMessage} onChange={e => setInputMessage(e)} />
-				<UiButton onClick={handleClickButton} />
-			</HStack>
-		</Card>
-	)
-}
+  return (
+    <Card className={cx(cls.Chat, {}, [className])}>
+      <VStack className={cls.messageLog}>
+        {messageHistory.map((message) => (
+          <UiText>{message.message}</UiText>
+        ))}
+      </VStack>
+      <HStack>
+        <UiInput value={inputMessage} onChange={(e) => setInputMessage(e)} />
+        <UiButton onClick={handleClickButton} />
+      </HStack>
+    </Card>
+  );
+};
