65cff18c8fc307b01322ed43604eee67d20dd9cc
Maksim Korolev <kimikirimusu@gmail.com>
2024-03-31 18:36:08 +0300

    AuthPage design upgraded

--- src/shared/ui/UiInput/ui/UiInput.tsx
+++ src/shared/ui/UiInput/ui/UiInput.tsx
@@ -1,5 +1,6 @@
-import { ChangeEvent, InputHTMLAttributes, ReactNode, useState } from "react";
+import { ChangeEvent, InputHTMLAttributes } from "react";
 import cls from "./UiInput.module.scss";
+import { UiText, VStack } from "../..";
 
 type HTMLInputProps = Omit<
   InputHTMLAttributes<HTMLInputElement>,
@@ -9,25 +10,31 @@ interface InputProps extends HTMLInputProps {
   className?: string;
   value?: string | number;
   placeholder?: string;
+  label?: string;
   onChange?: (value: string) => void;
 }
 export const UiInput = ({
   className,
   value,
   placeholder,
+  label,
   onChange,
   ...otherProps
 }: InputProps) => {
   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
     onChange?.(e.target.value);
   };
+
   return (
-    <input
-      value={value}
-      className={`${cls.Input} ${className}`}
-      placeholder={placeholder}
-      onChange={onChangeHandler}
-      {...otherProps}
-    />
+    <VStack align="start" gap="8">
+      {label && <UiText>{label}</UiText>}
+      <input
+        value={value}
+        className={`${cls.Input} ${className}`}
+        placeholder={placeholder}
+        onChange={onChangeHandler}
+        {...otherProps}
+      />
+    </VStack>
   );
 };
