fa38cc7b57b7494772935a6d06abf8e779221af1
Naitu2 <kimikirimusu@gmail.com>
2024-04-19 15:06:42 +0300

    Base server established

---
+++ TalkBackGameServer/index.js
@@ -0,0 +1,21 @@
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
+import http from 'http'
+import router from './online/online.routes.js'
+
+dotenv.config()
+
+const app = express()
+const server = http.createServer(app)
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/', router)
+
+const PORT = process.env.PORT || 4200
+
+server.listen(PORT, () => {
+	console.log(`Server listening at http://localhost:${PORT}`)
+})
