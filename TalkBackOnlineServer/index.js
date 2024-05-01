3181956eb9786939a9b61f184334f2313dd05a35
Naitu2 <kimikirimusu@gmail.com>
2024-05-01 16:36:00 +0300

    Watch added to all the servers

--- TalkBackOnlineServer/index.js
+++ TalkBackOnlineServer/index.js
@@ -1,10 +1,10 @@
-import cors from 'cors';
-import dotenv from 'dotenv';
-import express from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
-import { connectOnline } from './online/online.controller.js';
-import onlineRouter from './online/online.routes.js';
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import { connectOnline } from "./online/online.controller.js";
+import onlineRouter from "./online/online.routes.js";
 
 dotenv.config();
 
@@ -22,8 +22,7 @@ const PORT = process.env.PORT || 4000;
 async function main() {
   app.use(cors());
   app.use(express.json());
-  app.use('/api/online', onlineRouter);
-
+  app.use("/api/online", onlineRouter);
   // const __dirname = path.resolve();
   //app.use('/uploads', express.static(path.join(__dirname, '/uploads/')))
 }
