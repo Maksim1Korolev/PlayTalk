ea2af6ce3db95444a76440b7cb741f572e6654ff
Naitu2 <kimikirimusu@gmail.com>
2024-05-01 19:02:01 +0300

    GameWidget added with backgammon component

--- TalkBackClient/src/widgets/GameWidget/ui/GameWidget.tsx
+++ TalkBackClient/src/widgets/GameWidget/ui/GameWidget.tsx
@@ -1,11 +1,17 @@
-import { memo } from 'react'
-import cls from './GameWidget.module.scss'
+import cls from "./GameWidget.module.scss";
+import { cx } from "@/shared/lib/cx";
+import { Backgammon } from "./Backgammon";
 
-export const GameWidget = ({ className }: {className?: string}) => {
-		
-	return (
-		<div className={`${cls.GameWidget} ${className}`}>
-
-		</div>
-	)
-}
+export const GameWidget = ({
+  className,
+  inGame = false,
+}: {
+  className?: string;
+  inGame?: boolean;
+}) => {
+  return (
+    <div className={cx(cls.GameWidget, {}, [className])}>
+      {inGame ? <div>ZAGLUSHKA</div> : <Backgammon />}
+    </div>
+  );
+};
