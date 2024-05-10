1eb8a939703aba966559140c2d853fce2685ff7f
Naitu2 <kimikirimusu@gmail.com>
2024-05-10 14:35:35 +0300

    Disconnect bug fixed

--- TalkBackClient/src/pages/OnlinePage/hooks/useConnectionGameSocket.ts
+++ TalkBackClient/src/pages/OnlinePage/hooks/useConnectionGameSocket.ts
@@ -13,14 +13,31 @@ export const useConnectionGameSocket = ({
 
   const updateUsersGameStatus = useCallback(
     (usernames: string[], busy: boolean) => {
-      setUpToDateUsers(prev =>
-        prev?.map(user => ({
-          ...user,
-          inGame: usernames.includes(user.username) ? busy : user.inGame,
-        }))
-      );
+      console.log("Updating users game status:");
+      console.log("Usernames:", usernames);
+      console.log("Busy:", busy);
+
+      setUpToDateUsers(prev => {
+        const updatedUsers = prev?.map(user => {
+          if (usernames.includes(user.username)) {
+            console.log(
+              `Updating inGame status for user: ${user.username} to ${busy}`
+            );
+            return {
+              ...user,
+              inGame: busy,
+            };
+          }
+          return user;
+        });
+        console.log("Updated users:", updatedUsers);
+        return updatedUsers;
+      });
 
       if (usernames.includes(user.username)) {
+        console.log(
+          `Updating inGame status in cookie for user: ${user.username} to ${busy}`
+        );
         setCookie("jwt-cookie", {
           ...cookies["jwt-cookie"],
           user: {
@@ -30,26 +47,30 @@ export const useConnectionGameSocket = ({
         });
       }
     },
-    [setUpToDateUsers]
+    [setUpToDateUsers, setCookie, cookies, user.username]
   );
 
   const handleSendGameInvite = useCallback(
     ({ receiverUsername }: { receiverUsername: string }) => {
+      console.log(`Sending game invite to: ${receiverUsername}`);
       gameSocket.emit("send-game-invite", { receiverUsername });
     },
     [updateUsersGameStatus, user.username]
   );
 
   const handleAcceptGame = useCallback(() => {
+    console.log("Accepting game invite");
     gameSocket.emit("accept-game");
   }, []);
 
   const handleEndGame = useCallback(() => {
+    console.log("Ending game");
     gameSocket.emit("end-game");
   }, []);
 
   useEffect(() => {
     const onConnect = () => {
+      console.log(`Connected as user: ${user.username}`);
       gameSocket.emit("online-ping", user.username);
     };
 
@@ -71,7 +92,7 @@ export const useConnectionGameSocket = ({
       gameSocket.off("update-busy-status", updatePlayingUsersStatus);
       gameSocket.close();
     };
-  }, [updateUsersGameStatus, user.username]);
+  }, []);
 
   return {
     handleSendGameInvite,
