4b9814533f4f90064bbfead6e6d45c7741122f0e
Naitu2 <kimikirimusu@gmail.com>
2024-05-24 21:04:44 +0300

    mongoose removed from authServer, index.js corrected

--- TalkBackAuthServer/src/index.js
+++ TalkBackAuthServer/src/index.js
@@ -1,11 +1,9 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import mongoose from "mongoose";
 import path from "path";
 
 import authRoutes from "./auth/auth.routes.js";
-import UserService from "./services/UserService.js";
 import usersRoutes from "./users/users.routes.js";
 
 import { errorHandler, notFound } from "./middleware/error.middleware.js";
@@ -20,33 +18,23 @@ async function main() {
   const __dirname = path.resolve();
 
   app.use("/public", express.static(path.join(__dirname, "../public")));
-  console.log(__dirname);
 
   app.use("/api/auth", authRoutes);
   app.use("/api/users", usersRoutes);
 
   app.use(notFound);
   app.use(errorHandler);
+
+  const PORT = process.env.PORT || 3010;
+
+  app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+  });
 }
-const PORT = process.env.PORT || 3010;
-
-const mongoURL = process.env.DATABASE_URL;
-
-console.log(mongoURL);
-
-mongoose
-  .connect(mongoURL)
-  .then(() => console.log("Successfully connected to MongoDB"))
-  .catch(err => console.error("Connection error", err));
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
-});
 
 main()
   .then(async () => {})
   .catch(async e => {
     console.error(e);
-    await mongoose.disconnect();
     process.exit(1);
   });
