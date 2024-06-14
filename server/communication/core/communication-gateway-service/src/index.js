2b6d97e3a8e53ae78e48225fa4ec89ca6bdbcfdd
Naitu2 <kimikirimusu@gmail.com>
2024-06-14 18:40:39 +0300

    communication server index.js updated, sockerService file added, todos file updated

--- server/communication/core/communication-gateway-service/src/index.js
+++ server/communication/core/communication-gateway-service/src/index.js
@@ -3,9 +3,12 @@ import dotenv from "dotenv";
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+
 import chatRouter from "./chat/chat.routes.js";
-import { connectOnline } from "./connection/connection.controller.js";
 import onlineRouter from "./connection/connection.routes.js";
+import { connectOnline } from "./connection/connection.controller.js";
+
+import redisClient from "./utils/redisClient.js";
 
 dotenv.config();
 
@@ -16,21 +19,26 @@ export const io = new Server(server, {
   cors: {},
 });
 
-await connectOnline();
-
-const PORT = process.env.PORT || 3000;
-
 async function main() {
+  await connectOnline();
+  await redisClient.connect();
+
+  const PORT = process.env.PORT || 3000;
+
   app.use(cors());
   app.use(express.json());
   app.use("/api/connection", onlineRouter);
   app.use("/api/chat", chatRouter);
+
+  server.listen(PORT, () => {
+    console.log(
+      `communication-gateway-service is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    );
+  });
 }
 
-server.listen(PORT, () => {
-  console.log(
-    `communication-gateway-service is running in ${process.env.NODE_ENV} mode on port ${PORT}`
-  );
+main().catch(async e => {
+  console.error(e);
+  redisClient.quit();
+  process.exit(1);
 });
-
-main();
