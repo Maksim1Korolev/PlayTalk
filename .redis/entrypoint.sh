f016efdf05e8176c7f6ffdc006a8e0c01b4f3287
Maksim Korolev <maksimkorolev3600@gmail.com>
2024-07-08 18:35:56 +0300

    Socket and redis issue fixed

--- .redis/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Start Redis server in the background
-redis-server &
-# Wait for Redis server to start
-sleep 1
-# Flush all Redis data
-redis-cli FLUSHALL
-# Bring Redis server process to foreground
-fg
