40c00d49bd5aaf6ff4d769747ac01238d6acff7c
Maksim Korolev <maksimkorolev3600@gmail.com>
2024-07-30 13:36:04 +0300

    Player model added

---
+++ server/game/modules/TicTacToe/Models/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace TicTacToe.Models
+{
+    public class Player
+    {
+        public string Username { get; set; }
+        public int Wins { get; set; }
+        public char Sign { get; set; }
+    }
+}
