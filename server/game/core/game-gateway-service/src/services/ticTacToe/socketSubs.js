992d82a57a0892970422664930acedade42397be
Maksim Korolev <maksimkorolev3600@gmail.com>
2024-08-13 14:00:17 +0300

    socketSubs file initialized

---
+++ server/game/core/game-gateway-service/src/services/ticTacToe/socketSubs.js
@@ -0,0 +1,6 @@
+import SocketService from "../socketService.js";
+import { io } from "../../index.js";
+
+async function handleTicTacToeSubscriptions(socket, username) {}
+
+export default handleTicTacToeSubscriptions;
--- server/game/modules/tic-tac-toe/tic-tac-toe-repository-service/src/players/players.routes.js
+++ server/game/modules/tic-tac-toe/tic-tac-toe-repository-service/src/players/players.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getPlayer, getPlayers, updatePlayers } from "./players.controller.js";
+import { getPlayer, updatePlayers } from "./players.controller.js";
 
 const router = express.Router();
 
