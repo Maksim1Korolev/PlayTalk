140626bc9ac18c2b0e2036e9cd97c6f52e904391
Maksim Korolev <maksimkorolev3600@gmail.com>
2024-08-22 11:01:33 +0300

    todos updated

--- client/src/shared/api/sockets.ts
+++ client/src/shared/api/sockets.ts
@@ -2,4 +2,5 @@ import { io } from "socket.io-client";
 export const communicationSocket = io(
   import.meta.env.VITE_COMMUNICATION_SOCKET_URL
 );
+
 export const gameSocket = io(import.meta.env.VITE_GAME_SOCKET_URL);
