57eb50e5538e4c51c2c76bb9d20b878ee2dba2fd
Maksim Korolev <maksimkorolev3600@gmail.com>
2024-09-04 11:28:01 +0300

    todos and templates updated

--- .fttemplates/React Component/ui/[FTName].tsx
+++ .fttemplates/React Component/ui/[FTName].tsx
@@ -2,11 +2,11 @@ import { memo } from 'react'
 import cls from './<FTName>.module.scss'
 import { cx } from "@/shared/lib/cx";
 
-export const <FTName> = ({ className }: {className?: string}) => {
+export const <FTName> = memo(({ className }: {className?: string}) => {
 		
 	return (
 		<div className={cx(cls.<FTName>, {}, [className])}>
 
 		</div>
 	)
-}
+});
