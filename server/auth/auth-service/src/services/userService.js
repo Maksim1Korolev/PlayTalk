0c1766f02bb449b726c627dc7d9a584b98ee4d29
Maksim Korolev <maksimkorolev3600@gmail.com>
2024-09-07 15:32:42 +0300

    unused code removed from auth-service's UserService

--- server/auth/auth-service/src/services/userService.js
+++ server/auth/auth-service/src/services/userService.js
@@ -3,21 +3,9 @@ import axios from "axios";
 const repositoryServiceUrl = `${process.env.AUTH_REPOSITORY_SERVICE_URL}/users`;
 
 class UserService {
-  static getUsers = async () => {
-    const url = repositoryServiceUrl;
-    const response = await axios.get(url);
-    return response.data.users;
-  };
-
   static addUser = async user => {
     const url = repositoryServiceUrl;
     const response = await axios.post(url, { user });
-    return response.data.user;
-  };
-
-  static deleteUser = async userId => {
-    const url = `${repositoryServiceUrl}/${userId}`;
-    const response = await axios.delete(url);
     return response.data.user;
   };
 
@@ -25,18 +13,6 @@ class UserService {
     const response = await axios.get(
       `${repositoryServiceUrl}/username/${username}`
     );
-    return response.data.user;
-  };
-
-  static getUserById = async userId => {
-    const url = `${repositoryServiceUrl}/id/${userId}`;
-    const response = await axios.get(url);
-    return response.data.user;
-  };
-
-  static updateUser = async user => {
-    const url = `${repositoryServiceUrl}/${user._id}`;
-    const response = await axios.put(url, user);
     return response.data.user;
   };
 }
