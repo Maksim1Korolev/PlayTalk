b2d3c16fb77e81647f178a058c28f2866c18673e
Maksim Korolev <maksimkorolev3600@gmail.com>
2024-09-07 15:43:57 +0300

    userService file created on game gateway service

--- server/game/core/game-gateway-service/src/middleware/authMiddleware.js
+++ server/game/core/game-gateway-service/src/middleware/authMiddleware.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import jwt from "jsonwebtoken";
+import { io } from "../index.js";
 //import UserService from "../services/userService.js";
 
 export const protect = asyncHandler(async (req, res, next) => {
@@ -21,3 +22,38 @@ export const protect = asyncHandler(async (req, res, next) => {
   //   throw new Error("Not authorized, You don't have token");
   // }
 });
+
+// io.engine.use((req, res, next) => {
+//   const isHandshake = req._query.sid === undefined;
+//   if (!isHandshake) {
+//     return next();
+//   }
+
+//   const header = req.headers["authorization"];
+
+//   if (!header) {
+//     return next(new Error("no token"));
+//   }
+
+//   if (!header.startsWith("bearer ")) {
+//     return next(new Error("invalid token"));
+//   }
+
+//   const token = header.substring(7);
+
+//   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+//     if (err) {
+//       return next(new Error("invalid token"));
+//     }
+//     const userFound = await UserService.getUserById(decoded.userId);
+//     if (userFound) {
+//       req.user = userFound;
+//       next();
+//     } else {
+//       res.status(401);
+//       throw new Error("Not authorized");
+//     }
+//     req.user = decoded.data;
+//     next();
+//   });
+// });
