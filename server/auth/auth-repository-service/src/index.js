cd833b0cf8b8cc95dc013eafff6097a6b288c4a3
Maksim Korolev <maksimkorolev3600@gmail.com>
2024-09-09 18:13:53 +0300

    basic cors settings added, todos updated

--- server/auth/auth-repository-service/src/index.js
+++ server/auth/auth-repository-service/src/index.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 async function main() {
-  app.use(cors());
+  app.use(cors({ origin: "*" }));
   app.use(express.json());
 
   app.use("/api/users", userRouter);
