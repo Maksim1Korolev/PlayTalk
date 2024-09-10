652ad27f2819a76b6fb52169b551efe48ffa98c3
Maksim Korolev <maksimkorolev3600@gmail.com>
2024-09-10 16:23:13 +0300

    todos updated, cosmetic changes

--- server/auth/auth-repository-service/src/index.js
+++ server/auth/auth-repository-service/src/index.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 async function main() {
-  app.use(cors({ origin: "*" }));
+  app.use(cors());
   app.use(express.json());
 
   app.use("/api/users", userRouter);
