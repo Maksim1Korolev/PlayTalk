a9a133f94e689933aae5b7baf2bef7153f827118
Maksim Korolev <maksimkorolev3600@gmail.com>
2024-09-11 13:05:22 +0300

    authMiddleware on auth repo adjusted

--- server/auth/auth-repository-service/src/middleware/authMiddleware.js
+++ server/auth/auth-repository-service/src/middleware/authMiddleware.js
@@ -4,31 +4,29 @@ import jwt from "jsonwebtoken";
 import UserService from "../services/userService.js";
 
 export const protect = asyncHandler(async (req, res, next) => {
-  let token;
+  const authorizationHeader = req.headers.authorization;
 
-  if (req.headers.authorization?.startsWith("Bearer")) {
-    token = req.headers.authorization.split(" ")[1];
-
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      const userFound = await UserService.getUserById(decoded.userId);
-
-      if (userFound) {
-        req.user = userFound;
-        next();
-      } else {
-        res.status(401).json({ message: "User not found" });
-      }
-    } catch (error) {
-      res
-        .status(500)
-        .json({ message: "Error verifying token", error: error.message });
-    }
-  } else {
-    res.status(401).json({ message: "Not authorized, no token provided" });
+  if (!authorizationHeader || !authorizationHeader.startsWith("Bearer")) {
+    return res
+      .status(401)
+      .json({ message: "Not authorized, no token provided" });
   }
 
-  if (!token) {
-    res.status(401).json({ message: "Not authorized, no token provided" });
+  const token = authorizationHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userFound = await UserService.getUserById(decoded.userId);
+
+    if (!userFound) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    req.user = userFound;
+    next();
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error verifying token", error: error.message });
   }
 });
