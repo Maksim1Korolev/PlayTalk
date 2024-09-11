01f24773d44400cf0d5d00f8fb381b1c5a4fa907
Maksim Korolev <maksimkorolev3600@gmail.com>
2024-09-11 11:54:31 +0300

    logger file updated everywhere, winstone logger added to ttt repo

--- server/auth/auth-service/src/utils/logger.js
+++ server/auth/auth-service/src/utils/logger.js
@@ -1,33 +1,29 @@
 import { createLogger, format, transports } from "winston";
 import DailyRotateFile from "winston-daily-rotate-file";
 
-const { combine, timestamp, printf } = format;
+const { combine, timestamp, printf, label } = format;
 
-const customFormat = printf(({ level, message, label, timestamp }) => {
+const customFormat = printf(({ level, message, timestamp, label }) => {
   return `${timestamp} [${label}] [${level.toUpperCase()}]: ${message}`;
 });
 
-const logger = createLogger({
-  level: "info",
-  format: combine(timestamp(), customFormat),
-  transports: [
-    new transports.Console(),
-    new DailyRotateFile({
-      filename: "logs/application-%DATE%.log",
-      datePattern: "YYYY-MM-DD",
-      maxFiles: "14d",
-    }),
-    new DailyRotateFile({
-      filename: "logs/error-%DATE%.log",
-      level: "error",
-      datePattern: "YYYY-MM-DD",
-      maxFiles: "14d",
-    }),
-  ],
-});
-
-export const getLogger = labelName => {
-  return logger.child({ label: labelName });
+export const getLogger = (customLabel = "Default") => {
+  return createLogger({
+    level: "info",
+    format: combine(label({ label: customLabel }), timestamp(), customFormat),
+    transports: [
+      new transports.Console(),
+      new DailyRotateFile({
+        filename: "logs/application-%DATE%.log",
+        datePattern: "YYYY-MM-DD",
+        maxFiles: "14d",
+      }),
+      new DailyRotateFile({
+        filename: "logs/error-%DATE%.log",
+        level: "error",
+        datePattern: "YYYY-MM-DD",
+        maxFiles: "14d",
+      }),
+    ],
+  });
 };
-
-export default logger;
