f9d753a1fd95576bacc564bada4c4136ee34e422
Maksim Korolev <maksimkorolev3600@gmail.com>
2024-09-11 08:38:54 +0300

    Same logic implemented on chat repo

--- server/communication/modules/chat/chat-repository-service/src/index.js
+++ server/communication/modules/chat/chat-repository-service/src/index.js
@@ -9,6 +9,7 @@ import {
 } from "./utils/mongooseClient.js";
 
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+import { serviceWhitelistMiddleware } from "./middleware/serviceWhitelistMiddleware.js";
 
 import messageHistoryRouter from "./routes/messageHistoryRoutes.js";
 import unreadRouter from "./routes/unreadRoutes.js";
@@ -22,8 +23,12 @@ async function main() {
   app.use(cors());
   app.use(express.json());
 
-  app.use("/api/messageHistories", messageHistoryRouter);
-  app.use("/api/unread", unreadRouter);
+  app.use(
+    "/api/messageHistories",
+    serviceWhitelistMiddleware,
+    messageHistoryRouter
+  );
+  app.use("/api/unread", serviceWhitelistMiddleware, unreadRouter);
 
   app.use(errorHandler);
   app.use(notFound);
---
+++ server/communication/modules/chat/chat-repository-service/src/middleware/serviceWhitelistMiddleware.js
@@ -0,0 +1,14 @@
+const WHITELISTED_SERVICES = ["communication_gateway_service"];
+
+export const serviceWhitelistMiddleware = (req, res, next) => {
+  const serviceHeaderKey = process.env.INTERNAL_SERVICE_HEADER;
+  const serviceHeader = req.headers[serviceHeaderKey];
+
+  console.log("Request from: ", serviceHeader);
+
+  if (WHITELISTED_SERVICES.includes(serviceHeader)) {
+    next();
+  } else {
+    res.status(403).json({ message: "Access forbidden: Service not allowed" });
+  }
+};
