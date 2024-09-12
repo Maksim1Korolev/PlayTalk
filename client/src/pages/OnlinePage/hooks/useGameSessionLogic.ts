09e1858428517fd36f3049c7a95eb5ebd8ed639a
Maksim Korolev <maksimkorolev3600@gmail.com>
2024-09-12 14:44:48 +0300

    useGameSessionLogic hook created, logic moved

---
+++ client/src/pages/OnlinePage/hooks/useGameSessionLogic.ts
@@ -0,0 +1,143 @@
+import { useCallback, useState } from "react";
+import { Invite, getInviteKey } from "@/entities/Game/model";
+import { User } from "@/entities/User";
+import { useGameModals } from "./useGameModals";
+import { useGameSessionSocket } from "./useGameSessionSocket";
+
+type GameStartPayload = {
+  opponentUsername: string;
+  gameName: string;
+};
+
+type GameEndPayload = {
+  opponentUsername: string;
+  gameName: string;
+  winner: string;
+};
+
+export const useGameSessionLogic = (
+  users: User[],
+  updateUserList: (username: string, updatedProps: Partial<User>) => void
+) => {
+  const [inviteMap, setInviteMap] = useState<{ [key: string]: Invite }>({});
+  const [lastClickedPlayUser, setLastClickedPlayUser] = useState<User | null>(
+    null
+  );
+
+  const { gameModals, handleOpenGameModal, handleCloseGameModal } =
+    useGameModals();
+
+  const getUser = (username: string): User | undefined => {
+    return users.find(user => user.username === username);
+  };
+
+  const handleOpenGameSelector = useCallback((user: User) => {
+    setLastClickedPlayUser(user);
+  }, []);
+
+  const onReceiveInvite = (invite: Invite) => {
+    const inviteKey = getInviteKey(invite);
+
+    setInviteMap(prevInvites => ({
+      ...prevInvites,
+      [inviteKey]: invite,
+    }));
+
+    updateUserList(invite.senderUsername, {
+      isInviting: true,
+    });
+  };
+
+  const onGameStart = ({ opponentUsername, gameName }: GameStartPayload) => {
+    const user = getUser(opponentUsername); // Retrieve the user
+
+    updateUserList(opponentUsername, {
+      activeGames: [...(user?.activeGames || []), gameName],
+    });
+
+    handleOpenGameModal({ opponentUsername, gameName });
+  };
+
+  const onGameEnd = ({
+    opponentUsername,
+    gameName,
+    winner,
+  }: GameEndPayload) => {
+    const user = getUser(opponentUsername);
+
+    updateUserList(opponentUsername, {
+      activeGames: (user?.activeGames || []).filter(game => game !== gameName),
+    });
+
+    handleCloseGameModal({ opponentUsername, gameName });
+  };
+
+  const { handleSendGameInvite, handleAcceptGame } = useGameSessionSocket({
+    onReceiveInvite,
+    onGameStart,
+    onGameEnd,
+  });
+
+  const updateInvitingStatus = (senderUsername: string) => {
+    updateUserList(senderUsername, {
+      isInviting: false,
+    });
+  };
+
+  const handleGameRequestYesButton = (invite: Invite) => {
+    handleAcceptGame({
+      opponentUsername: invite.senderUsername,
+      gameName: invite.gameName,
+    });
+    const inviteKey = getInviteKey(invite);
+    setInviteMap(prevInvites => {
+      const { [inviteKey]: removed, ...remainingInvites } = prevInvites;
+      return remainingInvites;
+    });
+    updateInvitingStatus(invite.senderUsername);
+  };
+
+  const handleGameRequestNoButton = (invite: Invite) => {
+    const inviteKey = getInviteKey({
+      senderUsername: invite.senderUsername,
+      gameName: invite.gameName,
+    });
+    setInviteMap(prevInvites => {
+      const { [inviteKey]: removed, ...remainingInvites } = prevInvites;
+      return remainingInvites;
+    });
+    updateInvitingStatus(invite.senderUsername);
+  };
+
+  const handleGameClicked = ({
+    opponentUsername,
+    gameName,
+    isActive,
+    isInviting,
+  }: {
+    opponentUsername: string;
+    gameName: string;
+    isActive: boolean;
+    isInviting: boolean;
+  }) => {
+    if (isInviting) {
+      const invite: Invite = { senderUsername: opponentUsername, gameName };
+      handleGameRequestYesButton(invite);
+    } else if (isActive) {
+      handleOpenGameModal({ opponentUsername, gameName });
+    } else {
+      handleSendGameInvite({ receiverUsername: opponentUsername, gameName });
+    }
+  };
+
+  return {
+    lastClickedPlayUser,
+    invites: Object.values(inviteMap),
+    gameModals,
+    handleGameClicked,
+    handleGameRequestYesButton,
+    handleGameRequestNoButton,
+    handleOpenGameSelector,
+    onGameModalClose: handleCloseGameModal,
+  };
+};
--- client/src/pages/OnlinePage/hooks/useOnlinePageSockets.ts
+++ client/src/pages/OnlinePage/hooks/useOnlinePageSockets.ts
@@ -1,38 +1,16 @@
-import { useCallback, useContext, useEffect, useState } from "react";
+import { useCallback, useContext, useState, useEffect } from "react";
 import { useCookies } from "react-cookie";
 
 import { User } from "@/entities/User";
 import { useSockets } from "@/shared/hooks/useSockets";
-import { useGameModals } from "./useGameModals";
-import { useGameSessionSocket } from "./useGameSessionSocket";
 import { useOnlineSockets } from "./useOnlineSockets";
-import { Invite, getInviteKey } from "@/entities/Game/model";
 import { UsersContext } from "@/shared/lib/context/UsersContext";
+import { useGameSessionLogic } from "./useGameSessionLogic";
 
 export const useOnlinePageSockets = () => {
   const [cookies] = useCookies();
   const { user: currentUser } = cookies["jwt-cookie"];
   const { users, setUsers } = useContext(UsersContext);
-
-  const [inviteMap, setInviteMap] = useState<{ [key: string]: Invite }>({});
-  const [lastClickedPlayUser, setLastClickedPlayUser] = useState<User | null>(
-    null
-  );
-
-  useSockets();
-
-  const updateUsersForList = useCallback(
-    (userList: User[]) => {
-      if (!currentUser) return;
-
-      const otherUsers = userList.filter(
-        (user: User) => user._id !== currentUser._id
-      );
-
-      setUsers(otherUsers || []);
-    },
-    [currentUser, setUsers]
-  );
 
   const updateUserList = useCallback(
     (username: string, updatedProps: Partial<User>) => {
@@ -53,137 +31,42 @@ export const useOnlinePageSockets = () => {
     [setUsers]
   );
 
+  const {
+    lastClickedPlayUser,
+    invites,
+    gameModals,
+    handleGameClicked,
+    handleGameRequestYesButton,
+    handleGameRequestNoButton,
+    handleOpenGameSelector,
+    onGameModalClose,
+  } = useGameSessionLogic(users, updateUserList);
+
+  useSockets();
+
+  const updateUsersForList = useCallback(
+    (userList: User[]) => {
+      if (!currentUser) return;
+
+      const otherUsers = userList.filter(
+        (user: User) => user._id !== currentUser._id
+      );
+
+      setUsers(otherUsers || []);
+    },
+    [currentUser, setUsers]
+  );
+
   useOnlineSockets({
     updateUserList,
   });
 
-  const handleOpenGameSelector = useCallback((user: User) => {
-    setLastClickedPlayUser(user);
-  }, []);
-
-  const onReceiveInvite = (invite: Invite) => {
-    const inviteKey = getInviteKey(invite);
-
-    setInviteMap(prevInvites => ({
-      ...prevInvites,
-      [inviteKey]: invite,
-    }));
-
-    updateUserList(invite.senderUsername, {
-      isInviting: true,
-    });
-  };
-
-  const getUser = (username: string): User | undefined => {
-    return users?.find(user => user.username === username);
-  };
-
-  const onGameStart = ({
-    opponentUsername,
-    gameName,
-  }: {
-    opponentUsername: string;
-    gameName: string;
-  }) => {
-    updateUserList(opponentUsername, {
-      activeGames: [
-        ...(getUser(opponentUsername)?.activeGames || []),
-        gameName,
-      ],
-    });
-
-    handleOpenGameModal({ opponentUsername, gameName });
-  };
-
-  const onGameEnd = ({
-    opponentUsername,
-    gameName,
-    winner,
-  }: {
-    opponentUsername: string;
-    gameName: string;
-    winner: string;
-  }) => {
-    updateUserList(opponentUsername, {
-      activeGames: (getUser(opponentUsername)?.activeGames || []).filter(
-        game => game !== gameName
-      ),
-    });
-
-    handleCloseGameModal({ opponentUsername, gameName });
-  };
-
-  const { gameModals, handleOpenGameModal, handleCloseGameModal } =
-    useGameModals();
-
-  const { handleSendGameInvite, handleAcceptGame } = useGameSessionSocket({
-    onReceiveInvite,
-    onGameStart,
-    onGameEnd,
-  });
-
-  const updateInvitingStatus = (senderUsername: string) => {
-    setUsers(prevUsers =>
-      prevUsers.map(user =>
-        user.username === senderUsername ? { ...user, isInviting: false } : user
-      )
-    );
-  };
-
-  const handleGameRequestYesButton = (invite: Invite) => {
-    handleAcceptGame({
-      opponentUsername: invite.senderUsername,
-      gameName: invite.gameName,
-    });
-    const inviteKey = getInviteKey(invite);
-    setInviteMap(prevInvites => {
-      const { [inviteKey]: removed, ...remainingInvites } = prevInvites;
-      return remainingInvites;
-    });
-    updateInvitingStatus(invite.senderUsername);
-  };
-
-  const handleGameRequestNoButton = (invite: Invite) => {
-    const inviteKey = getInviteKey({
-      senderUsername: invite.senderUsername,
-      gameName: invite.gameName,
-    });
-    setInviteMap(prevInvites => {
-      const { [inviteKey]: removed, ...remainingInvites } = prevInvites;
-      return remainingInvites;
-    });
-    updateInvitingStatus(invite.senderUsername);
-  };
-
-  const handleGameClicked = ({
-    opponentUsername,
-    gameName,
-    isActive,
-    isInviting,
-  }: {
-    opponentUsername: string;
-    gameName: string;
-    isActive: boolean;
-    isInviting: boolean;
-  }) => {
-    if (isInviting) {
-      const invite: Invite = { senderUsername: opponentUsername, gameName };
-      handleGameRequestYesButton(invite);
-    } else if (isActive) {
-      handleOpenGameModal({ opponentUsername, gameName });
-    } else {
-      handleSendGameInvite({ receiverUsername: opponentUsername, gameName });
-    }
-
-    setLastClickedPlayUser(null);
-  };
-
   return {
     users,
-    invites: Object.values(inviteMap),
+    invites,
     lastClickedPlayUser,
     gameModals,
-    onGameModalClose: handleCloseGameModal,
+    onGameModalClose,
     updateUsers: updateUsersForList,
     handleOpenGameSelector,
     handleGameClicked,
