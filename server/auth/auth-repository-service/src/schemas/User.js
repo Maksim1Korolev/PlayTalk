e74750fe97ca504a374de1fd83d0d3d1e890a6dd
Maksim Korolev <maksimkorolev3600@gmail.com>
2024-09-24 17:24:24 +0300

    tests for userService added, mocks updated

--- server/auth/auth-repository-service/src/schemas/User.js
+++ server/auth/auth-repository-service/src/schemas/User.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 
-//TODO:Add required to avatarFileName
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true, maxlength: 15 },
   password: { type: String, required: true },
