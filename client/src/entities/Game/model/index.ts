6df6fe3d07f59a0f6af196589d062a8308d58db6
Michael Nakhimson <michael.nakhimson@gmail.com>
2024-10-17 10:36:41 +0300

    fix

--- client/src/entities/Game/model/index.ts
+++ client/src/entities/Game/model/index.ts
@@ -1,5 +1,6 @@
 export { GameNames } from "./enums/gameNames";
 export type { Game, TicTacToeGame } from "./types/Game";
+export { isGameName } from "./types/gameModalStateProps";
 export type {
   GameData,
   GameModalStateProps,
