10e55d52ce6694be86c17d52f4dc816f250f6931
Maksim Korolev <maksimkorolev3600@gmail.com>
2024-10-19 14:25:43 +0300

    user context and provider removed

--- client/src/app/providers/index.ts
+++ client/src/app/providers/index.ts
@@ -1,5 +1,4 @@
 export { SocketProvider } from "./SocketProvider";
-export { UserProvider } from "./UserProvider";
 export { StoreProvider } from "./StoreProvider/ui/StoreProvider";
 export { RequireAuth } from "./router/ui/RequireAuth";
 export { default as AppRouter } from "./router/ui/AppRouter";
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,10 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./app/App";
-import { StoreProvider, SocketProvider, UserProvider } from "./app/providers";
-import { ModalProvider } from "./app/providers/ModalProvider";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./app/App";
+import { SocketProvider, StoreProvider } from "./app/providers";
+import { ModalProvider } from "./app/providers/ModalProvider";
 import { store } from "./app/providers/StoreProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -11,11 +12,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
       <StoreProvider>
         <SocketProvider>
-          <UserProvider>
-            <ModalProvider>
-              <App />
-            </ModalProvider>
-          </UserProvider>
+          <ModalProvider>
+            <App />
+          </ModalProvider>
         </SocketProvider>
       </StoreProvider>
     </Provider>
