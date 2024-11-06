2b729199ef98958599957215bdd50306c63883cd
Maksim Korolev <maksimkorolev3600@gmail.com>
2024-11-06 09:49:15 +0200

    unused unread route removed

--- server/communication/core/communication-gateway-service/src/routes/unreadRoutes.js
+++ server/communication/core/communication-gateway-service/src/routes/unreadRoutes.js
@@ -2,16 +2,10 @@ import express from "express";
 
 import { protect } from "../middleware/authMiddleware.js";
 
-import {
-  getAllUnreadMessageCounts,
-  readAllUnreadMessages,
-} from "../controllers/unreadController.js";
+import { getAllUnreadMessageCounts } from "../controllers/unreadController.js";
 
 const router = express.Router();
 
-router
-  .route("/markAsRead/:recipientUsername")
-  .post(protect, readAllUnreadMessages);
 router.route("/getAll").get(protect, getAllUnreadMessageCounts);
 
 export default router;
