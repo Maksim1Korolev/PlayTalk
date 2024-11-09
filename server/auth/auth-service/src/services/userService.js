e8b774e691c798a108d82f4cf8096d69412267ac
Maksim Korolev <maksimkorolev3600@gmail.com>
2024-11-09 13:42:30 +0200

    unused functions moved on userService

--- server/auth/auth-service/src/services/userService.js
+++ server/auth/auth-service/src/services/userService.js
@@ -32,7 +32,7 @@ class UserService {
 
       if (cachedUsers && Object.keys(cachedUsers).length > 0) {
         logger.info("Fetched users from Redis cache.");
-        return Object.values(cachedUsers).map(user => {
+        return Object.values(cachedUsers).map((user) => {
           const { password, ...userWithoutPassword } = JSON.parse(user);
           return userWithoutPassword;
         });
@@ -51,41 +51,6 @@ class UserService {
     } catch (error) {
       logger.error(`Error fetching users: ${error.message}`);
       throw new Error("Failed to fetch users");
-    }
-  }
-
-  static async getUserById(id) {
-    if (!id || !mongoose.Types.ObjectId.isValid(id)) {
-      const error = "Invalid user ID";
-      logger.error(error);
-      throw new Error(error);
-    }
-
-    try {
-      const cachedUser = await redisClient.hGet(
-        process.env.REDIS_USERS_ID_KEY,
-        id
-      );
-
-      if (cachedUser) {
-        logger.info(`Cache hit for user with ID: ${id}`);
-        const { password, ...userWithoutPassword } = JSON.parse(cachedUser);
-        return userWithoutPassword;
-      }
-
-      const user = await User.findById(id).select("-password");
-      if (user) {
-        await redisClient.hSet(
-          process.env.REDIS_USERS_ID_KEY,
-          user._id.toString(),
-          JSON.stringify(user)
-        );
-        logger.info(`Fetched and cached user with ID: ${id}`);
-      }
-      return user;
-    } catch (error) {
-      logger.error(`Error fetching user by ID: ${error.message}`);
-      throw new Error("Failed to fetch user by ID");
     }
   }
 
@@ -136,40 +101,9 @@ class UserService {
     }
   }
 
-  static async updateUser(user) {
-    if (!user._id) {
-      const error = "Invalid user ID";
-      logger.error(error);
-      throw new Error(error);
-    }
+  //Unused functions for now
 
-    try {
-      const updatedUser = await User.findByIdAndUpdate(user._id, user, {
-        new: true,
-      }).select("-password");
-
-      if (updatedUser) {
-        await redisClient.hSet(
-          process.env.REDIS_USERS_USERNAME_KEY,
-          updatedUser.username,
-          JSON.stringify(updatedUser)
-        );
-        await redisClient.hSet(
-          process.env.REDIS_USERS_ID_KEY,
-          updatedUser._id.toString(),
-          JSON.stringify(updatedUser)
-        );
-        logger.info(`Updated and cached user: ${updatedUser.username}`);
-      }
-
-      return updatedUser;
-    } catch (error) {
-      logger.error(`Error updating user: ${error.message}`);
-      throw new Error("Failed to update user");
-    }
-  }
-
-  static async deleteUser(id) {
+  static async getUserById(id) {
     if (!id || !mongoose.Types.ObjectId.isValid(id)) {
       const error = "Invalid user ID";
       logger.error(error);
@@ -177,21 +111,65 @@ class UserService {
     }
 
     try {
-      const deletedUser = await User.findByIdAndDelete(id);
-      if (deletedUser) {
-        await redisClient.hDel(
-          process.env.REDIS_USERS_USERNAME_KEY,
-          deletedUser.username
-        );
-        await redisClient.hDel(process.env.REDIS_USERS_ID_KEY, deletedUser._id);
-        logger.info(`Deleted user: ${deletedUser.username}`);
+      const cachedUser = await redisClient.hGet(
+        process.env.REDIS_USERS_ID_KEY,
+        id
+      );
+
+      if (cachedUser) {
+        logger.info(`Cache hit for user with ID: ${id}`);
+        const { password, ...userWithoutPassword } = JSON.parse(cachedUser);
+        return userWithoutPassword;
       }
-      return deletedUser;
+
+      const user = await User.findById(id).select("-password");
+      if (user) {
+        await redisClient.hSet(
+          process.env.REDIS_USERS_ID_KEY,
+          user._id.toString(),
+          JSON.stringify(user)
+        );
+        logger.info(`Fetched and cached user with ID: ${id}`);
+      }
+      return user;
     } catch (error) {
-      logger.error(`Error deleting user: ${error.message}`);
-      throw new Error("Failed to delete user");
+      logger.error(`Error fetching user by ID: ${error.message}`);
+      throw new Error("Failed to fetch user by ID");
     }
   }
+
+  // static async updateUser(user) {
+  //   if (!user._id) {
+  //     const error = "Invalid user ID";
+  //     logger.error(error);
+  //     throw new Error(error);
+  //   }
+
+  //   try {
+  //     const updatedUser = await User.findByIdAndUpdate(user._id, user, {
+  //       new: true,
+  //     }).select("-password");
+
+  //     if (updatedUser) {
+  //       await redisClient.hSet(
+  //         process.env.REDIS_USERS_USERNAME_KEY,
+  //         updatedUser.username,
+  //         JSON.stringify(updatedUser)
+  //       );
+  //       await redisClient.hSet(
+  //         process.env.REDIS_USERS_ID_KEY,
+  //         updatedUser._id.toString(),
+  //         JSON.stringify(updatedUser)
+  //       );
+  //       logger.info(`Updated and cached user: ${updatedUser.username}`);
+  //     }
+
+  //     return updatedUser;
+  //   } catch (error) {
+  //     logger.error(`Error updating user: ${error.message}`);
+  //     throw new Error("Failed to update user");
+  //   }
+  // }
 }
 
 export default UserService;
