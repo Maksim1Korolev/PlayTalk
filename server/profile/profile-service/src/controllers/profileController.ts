15685e84fc3c4e281df657501196aed1e5a2c73c
Maksim Korolev <maksimkorolev3600@gmail.com>
2024-11-10 20:07:01 +0200

    unused profile routes removed from client and the service, todos updated

--- server/profile/profile-service/src/controllers/profileController.ts
+++ server/profile/profile-service/src/controllers/profileController.ts
@@ -6,42 +6,6 @@ import ProfileService from "../services/profileService";
 
 const logger = getLogger("ProfileController");
 
-// @desc   Add profile
-// @route  POST /api/profiles
-// @access Public
-export const addProfile = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<void> => {
-  const { username } = req.body;
-
-  if (!username) {
-    logger.warn("Username missing in add profile request");
-    res.status(400).json({ message: "Username is required" });
-    return;
-  }
-
-  try {
-    logger.info(`Attempt to add profile with username: ${username}`);
-    const profile = await ProfileService.addProfile(username);
-
-    if (!profile) {
-      logger.error("Failed to add profile");
-      res.status(500).json({ message: "Failed to add profile" });
-      return;
-    }
-
-    logger.info(`Profile added successfully: ${username}`);
-    res.status(201).json({ profile });
-  } catch (error: any) {
-    logger.error(
-      `Error adding profile for username: ${username} - ${error.message}`
-    );
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
-
 // @desc   Get profiles
 // @route  GET /api/profiles
 // @access Public
--- server/profile/profile-service/src/routes/profileRoutes.ts
+++ server/profile/profile-service/src/routes/profileRoutes.ts
@@ -1,14 +1,12 @@
 import express from "express";
 
 import {
-  addProfile,
   getProfileByUsername,
   getProfiles,
 } from "../controllers/profileController";
 
 const router = express.Router();
 
-router.route("/").post(addProfile);
 router.route("/").get(getProfiles);
 router.get("/:username", getProfileByUsername);
 
